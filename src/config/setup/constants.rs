d9b2c1ccf4768565a226f5af974d3b6f4dcd98d0
Youcef Kadri <youcef.kadri@kraken.tech>
2025-04-14 22:10:57 +1200

    Create config directory and default file

---
+++ src/config/setup/constants.rs
@@ -0,0 +1,3 @@
+pub const CONFIG_DIR: &str = "~/.config/idea";
+pub const CONFIG_FILE: &str = "config.toml";
+pub const DEFAULT_CONFIG: &str = include_str!("config.toml");
---
+++ src/config/setup/mod.rs
@@ -0,0 +1,25 @@
+use shellexpand;
+use std::io::Error;
+
+mod constants;
+
+pub fn setup() -> Result<(), Error> {
+    let binding = shellexpand::tilde(constants::CONFIG_DIR);
+    let config_dir = binding.as_ref();
+
+    if !std::path::Path::new(&config_dir).exists() {
+        println!("It looks like you haven't set up idea yet. Let's do that now...");
+
+        // Create the config directory
+        std::fs::create_dir_all(config_dir)?;
+
+        // Create the config file
+        let config_file_path = format!("{}/{}", &config_dir, constants::CONFIG_FILE);
+        let mut file = std::fs::File::create(&config_file_path)?;
+        std::io::Write::write_all(&mut file, constants::DEFAULT_CONFIG.as_bytes())?;
+
+        println!("Config file created at: {}", config_file_path);
+    }
+
+    Ok(())
+}
